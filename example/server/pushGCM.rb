2288692e68d5dcf3796796435893d3865ee72222
wasura4 <wasura.ediri@gmail.com>
2017-03-07 14:39:59 +0800

    Update pushGCM.rb

--- example/server/pushGCM.rb
+++ example/server/pushGCM.rb
@@ -2,8 +2,8 @@ require 'rubygems'
 require 'pushmeup'
 GCM.host = 'https://android.googleapis.com/gcm/send'
 GCM.format = :json
-GCM.key = "API_KEY_GOES_HERE"
-destination = ["REGISTRATION_ID_GOES_HERE"]
+GCM.key = "AIzaSyA7Z9QpxHIa3jWlAkVxAMKIVDCu2SpBbu4"
+destination = ["450314287423"]
 data = {:message => "PhoneGap Build rocks!", :msgcnt => "1", :soundname => "beep.wav"}
 
 GCM.send_notification( destination, data)
